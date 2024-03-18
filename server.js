64665261ef8276afaca9c1e08beabb14f077fa79
manu <manuelmonjes@gmail.com>
2024-03-18 11:31:11 -0300

    agregando comandos git a la pauta y add middleware al server

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
+	next();
+});
+
 app.get('/', (req, res) => {
 	res.send('hola mundo');
 });
