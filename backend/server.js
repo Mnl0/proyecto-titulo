37248e9f6e322767b189d4889efcae308aafb329
manu <manuelmonjes@gmail.com>
2024-03-19 13:17:28 -0300

    re estructuracion de carpetas

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+
+const app = express()
+
+const PORT = process.env.PORT ?? 3000;
+
+app.use(express.static('views'))
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
+	next();
+});
+
+
+app.get('/', (req, res) => {
+	res.send('hola mundo');
+});
+
+app.listen(PORT, () => {
+	console.log(`servidor corriendo en el puerto ${PORT}`)
+})
