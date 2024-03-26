988300c81efd5ac979202e663a5c22f5700f0057
manu <manuelmonjes@gmail.com>
2024-03-26 19:24:53 -0300

    feat:db schema sequilize

---
+++ backend/models/clienteModel.js
@@ -0,0 +1,16 @@
+// import { Sequelize, DataTypes } from "sequelize";
+
+// const sequilize = new Sequelize('mysql://root:1234@localhost:3306/prueba');
+
+// export const Cliente = sequilize.define('cliente_prueba', {
+// 	id: {
+// 		type: DataTypes.INTEGER,
+// 		// allowNull: true,
+// 	},
+// 	nombre: {
+// 		type: DataTypes.STRING,
+// 		// allowNull: true,
+// 	},
+// })
+
+
