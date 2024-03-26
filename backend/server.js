8a8baef008e894c3c3f538406680a333bb5073fc
manu <manuelmonjes@gmail.com>
2024-03-26 13:58:59 -0300

    feat(server) add coneccion bd and variable env

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,53 @@
 import express from 'express'
+import mysql from 'mysql'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
+const PORT = process.env.PORT;
+const connection = mysql.createConnection({
+	host: process.env.HOST,
+	user: process.env.USER,
+	password: process.env.PASS,
+	database: process.env.DATABASE,
+	port: process.env.DB_PORT
+})
 
-const PORT = process.env.PORT ?? 3000;
 
+//middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Content-Type');
-	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
-	next();
-});
 
+///estudiar bien los cors
+// app.use('/pathEspecifica', (req, res, next) => {
+// 	res.setHeader('Access-Control-Allow-Origin', '*');
+// 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+// 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
+// 	next();
+// });
 
-app.get('/', (req, res) => {
-	res.send('hola mundo');
-});
+const query = 'SELECT * FROM cliente_prueba'
+connection.query(query, function (err, rows, fields) {
+	if (err) {
+		console.log('error en la consulta', err)
+		return;
+	} else {
+		rows.forEach(row => {
+			console.log(row)
+		})
+		connection.end()
+	}
+})
+
 
 app.listen(PORT, () => {
 	console.log(`servidor corriendo en el puerto ${PORT}`)
+	connection.connect(function (err) {
+		if (err) {
+			console.log('error de conexion')
+			return;
+		}
+		console.log('conexion establecida')
+	})
 })
+
