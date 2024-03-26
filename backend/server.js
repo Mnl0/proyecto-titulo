d507c5a5e887eb6d581d184a6b07506829c374ab
manu <manuelmonjes@gmail.com>
2024-03-26 19:24:05 -0300

    feat:server add method get and post config cors

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import { connection } from './database/connection.js'
+import cors from 'cors'
+// import { Cliente } from './models/clienteModel.js'
 dotenv.config()
 
 const app = express()
@@ -9,25 +11,43 @@ const PORT = process.env.PORT;
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-///estudiar bien los cors
-// app.use('/pathEspecifica', (req, res, next) => {
-// 	res.setHeader('Access-Control-Allow-Origin', '*');
-// 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-// 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
-// 	next();
-// });
-
-const query = 'SELECT * FROM cliente_prueba'
-connection.query(query, function (err, rows, fields) {
-	if (err) {
-		console.log('error en la consulta', err)
-		return;
-	} else {
-		rows.forEach(row => {
-			console.log(row)
-		})
+app.use(cors(
+	{
+		origin: '*',
+		methods: ['GET', 'POST'],
+		allowedHeaders: ['Content-Type', 'Authorization'],
+		// exposedHeaders: ['header-personalizado'],
+		// maxAge: 10000,//tiempo en segundos que se va a guardar la peticion
 	}
+))
+
+// app.use('*', (req, res, next) => {
+// 	res.setHeader('Content-Type', 'application/json')
+// 	res.setHeader('Access-Control-Allow-Origin', '*')
+// 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
+// })
+
+app.get('/authentication', (req, res) => {
+	res.send('login de autenticacion')
+})
+
+const query = 'INSERT INTO cliente_prueba (nombre_completo) VALUES (?)'
+
+app.post('/authentication', (req, res) => {
+	const value = req.body.username
+
+	if (value === '') {
+		console.log('esta vacio')
+		return
+
+	}
+	connection.query(query, [value], function (err, result) {
+		if (err) {
+			console.log('error en la consulta', err)
+			return;
+		}
+		console.log('valor de resultados', result.affectedRows)
+	})
 })
 
 
