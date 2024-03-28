0f8d8ba43763162036f41e65a5e7cdbba400b0ef
manu <manuelmonjes@gmail.com>
2024-03-28 10:34:32 -0300

    refactor(server): new connection with sequilize for MVC pattern

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import { connection } from './database/connection.js'
 import cors from 'cors'
-// import { Cliente } from './models/clienteModel.js'
+import { sequelize } from './database/connection.js'
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT;
 
-//middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors(
@@ -16,49 +14,39 @@ app.use(cors(
 		origin: '*',
 		methods: ['GET', 'POST'],
 		allowedHeaders: ['Content-Type', 'Authorization'],
-		// exposedHeaders: ['header-personalizado'],
-		// maxAge: 10000,//tiempo en segundos que se va a guardar la peticion
 	}
 ))
 
-// app.use('*', (req, res, next) => {
-// 	res.setHeader('Content-Type', 'application/json')
-// 	res.setHeader('Access-Control-Allow-Origin', '*')
-// 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
+// app.get('/authentication', (req, res) => {
+// 	res.send('login de autenticacion')
 // })
 
-app.get('/authentication', (req, res) => {
-	res.send('login de autenticacion')
-})
+// const query = 'INSERT INTO cliente_prueba (nombre_completo) VALUES (?)'
 
-const query = 'INSERT INTO cliente_prueba (nombre_completo) VALUES (?)'
+// app.post('/authentication', (req, res) => {
+// 	const value = req.body.username
 
-app.post('/authentication', (req, res) => {
-	const value = req.body.username
+// 	if (value === '') {
+// 		console.log('esta vacio')
+// 		return
 
-	if (value === '') {
-		console.log('esta vacio')
-		return
-
-	}
-	connection.query(query, [value], function (err, result) {
-		if (err) {
-			console.log('error en la consulta', err)
-			return;
-		}
-		console.log('valor de resultados', result.affectedRows)
-	})
-})
-
+// 	}
+// 	connection.query(query, [value], function (err, result) {
+// 		if (err) {
+// 			console.log('error en la consulta', err)
+// 			return;
+// 		}
+// 		console.log('valor de resultados', result.affectedRows)
+// 	})
+// })
+try {
+	await sequelize.authenticate()
+	console.log('conexion exitosa')
+} catch (error) {
+	console.error('error en la conexion', error)
+}
 
 app.listen(PORT, () => {
 	console.log(`servidor corriendo en el puerto ${PORT}`)
-	connection.connect(function (err) {
-		if (err) {
-			console.log('error de conexion')
-			return;
-		}
-		console.log('conexion establecida')
-	})
 })
 
