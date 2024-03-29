f0f56f3f0fab0685b5f6818d52acdf2267308079
manu <manuelmonjes@gmail.com>
2024-03-29 18:09:04 -0300

    feat(controller): add delete method controller

--- backend/controllers/clientController.js
+++ backend/controllers/clientController.js
@@ -23,8 +23,19 @@ export const clientSchema = {
 	},
 	delete: (req, res) => {
 		const { id } = req.params
-		console.log(id)
-		return res.status(200).json({ message: 'llego al controlador ok' })
+		// console.log('este console log es de mi path delete', id)
+		if (id === '') return
+		if (id === undefined) return
+
+		const deleteQuery = 'DELETE FROM client WHERE emailclient = ?'
+		Client.sequelize.query(deleteQuery, {
+			replacements: [id],
+			type: Client.sequelize.QueryTypes.DELETE
+		}).then((resp) => {
+			res.status(200).json({ message: 'usuario eliminado', data: resp })
+		}).catch((error) => {
+			res.status(500).json({ message: 'ocurrio algo inesperado', error: error })
+		})
 	}
 }
 //localhost:3000/login/authentication
