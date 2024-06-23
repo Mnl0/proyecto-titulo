87aab9c6e9e06e7fb28576f652283777b7c03d9e
Victor Venegas <victor.venegas1@virginiogomez.cl>
2024-06-23 12:31:52 -0400

    FEATURES:(FRONT) Se complementa el chat con información del servicio solicitado.

--- front/src/views/components/workerCards/card/WorkerCard.jsx
+++ front/src/views/components/workerCards/card/WorkerCard.jsx
@@ -60,7 +60,7 @@ const WorkerCard = ( { user , onWorkerClick }) => {
                     {user.online ? <p>En línea</p> : <p>Fuera de línea</p>}
                     <div className={styles.ratingstarscontainer}>
                         <input value="star-5" name="star" id="star-5" type="radio" />
-                        <label for="star-5" className={styles.starlabel}>
+                        <label htmlFor="star-5" className={styles.starlabel}>
                             <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path
                                 d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"
@@ -70,7 +70,7 @@ const WorkerCard = ( { user , onWorkerClick }) => {
                         </label>
 
                         <input value="star-4" name="star" id="star-4" type="radio" />
-                        <label for="star-4" className={styles.starlabel}>
+                        <label htmlFor="star-4" className={styles.starlabel}>
                             <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path
                                 d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"
@@ -80,7 +80,7 @@ const WorkerCard = ( { user , onWorkerClick }) => {
                         </label>
 
                         <input value="star-3" name="star" id="star-3" type="radio" />
-                        <label for="star-3" className={styles.starlabel}>
+                        <label htmlFor="star-3" className={styles.starlabel}>
                             <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path
                                 d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"
@@ -90,7 +90,7 @@ const WorkerCard = ( { user , onWorkerClick }) => {
                         </label>
 
                         <input value="star-2" name="star" id="star-2" type="radio" />
-                        <label for="star-2" className={styles.starlabel}>
+                        <label htmlFor="star-2" className={styles.starlabel}>
                             <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path
                                 d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"
@@ -100,7 +100,7 @@ const WorkerCard = ( { user , onWorkerClick }) => {
                         </label>
 
                         <input value="star-1" name="star" id="star-1" type="radio" />
-                        <label for="star-1" className={styles.starlabel}>
+                        <label htmlFor="star-1" className={styles.starlabel}>
                             <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path
                                 d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"
